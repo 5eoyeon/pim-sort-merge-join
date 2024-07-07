5e714b3b218993a9f0e42c1f7ae36c88d1a8e1b2
5eoyeon <tjdus092222@gmail.com>
2024-07-07 22:14:41 +0900

    2024.06.dump-seoyeon

---
+++ dump-seoyeon/create_tasklet.c
@@ -0,0 +1,23 @@
+// dpu-upmem-dpurte-clang -DNR_TASKLETS=24 -o create_tasklet create_tasklet.c
+
+#include <stdio.h>
+#include <barrier.h>
+#include <defs.h>
+#include <stdint.h>
+
+#define NR_TASKLETS 2 // -> 테이블 쪼개서 데이터 나눠 갖기
+
+BARRIER_INIT(my_barrier, NR_TASKLETS);
+
+int result[NR_TASKLETS];
+
+int main() {
+    uint32_t tasklet_id = me();
+
+    result[tasklet_id] = tasklet_id;
+    printf("Tasklet %d is running\n", tasklet_id);
+
+    barrier_wait(&my_barrier);
+
+    return 0;
+}

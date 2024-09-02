dfba4ce27db4d618fdb0cf83fd7c9abd850465bc
5eoyeon <tjdus092222@gmail.com>
2024-09-02 19:03:28 +0900

    add: binary search

--- sort-merge-join/merge_dpu.c
+++ sort-merge-join/merge_dpu.c
@@ -20,22 +20,45 @@ uint32_t addr[NR_TASKLETS];
 int rows[NR_TASKLETS];
 int used_idx[NR_TASKLETS];
 
-void binary_search(){}
+int binary_search(uint32_t *base_addr, int col_num, int row_num, int target) {
+    int left = 0;
+    int right = row_num - 1;
+    int idx = -1;
+
+    int *mid_row = (int *)mem_alloc(col_num * sizeof(int));
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        mram_read((__mram_ptr void *)(base_addr + mid * col_num), mid_row, col_num * sizeof(int));
+        
+        if (mid_row[JOIN_KEY] == target) return idx;
+        else if (mid_row[JOIN_KEY] < target) {
+            idx = mid;
+            left = mid + 1;
+        } else right = mid - 1;
+    }
+
+    return idx;
+}
 
 int main()
 {
-    int col_num = bl.col_num;
-    int row_num = bl.row_num;
+    int col_num1 = bl1.col_num;
+    int row_num1 = bl1.row_num;
+    int col_num2 = bl1.col_num;
+    int row_num2 = bl1.row_num;
 
+    uint32_t mram_base_addr_dpu2 = (uint32_t)DPU_MRAM_HEAP_POINTER + row_num1 * col_num1 * sizeof(int);
+    
     unsigned int tasklet_id = me();
 
-    int row_per_tasklet = row_num / NR_TASKLETS;
-    int chunk_size = row_per_tasklet * col_num;
+    int row_per_tasklet = row_num1 / NR_TASKLETS;
+    int chunk_size = row_per_tasklet * col_num1;
     int start = tasklet_id * chunk_size;
     if (tasklet_id == NR_TASKLETS - 1)
     {
-        row_per_tasklet = row_num - (NR_TASKLETS - 1) * row_per_tasklet;
-        chunk_size = row_per_tasklet * col_num;
+        row_per_tasklet = row_num1 - (NR_TASKLETS - 1) * row_per_tasklet;
+        chunk_size = row_per_tasklet * col_num1;
     }
 
     uint32_t mram_base_addr = (uint32_t)DPU_MRAM_HEAP_POINTER + start * sizeof(int);
@@ -43,7 +66,9 @@ int main()
     rows[tasklet_id] = row_per_tasklet;
 
     /* do binary search */
-
+    int *last_row = (int *)mem_alloc(col_num1 * sizeof(int));
+    mram_read((__mram_ptr void *)(mram_base_addr + row_per_tasklet - 1 * col_num1 * sizeof(int)), last_row, col_num1 * sizeof(int));
+    used_idx[tasklet_id] = binary_search(mram_base_addr_dpu2, row_num2, col_num2, last_row[JOIN_KEY]);
     barrier_wait(&my_barrier);
     /* do merge sort */
 

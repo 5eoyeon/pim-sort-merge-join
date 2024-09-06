da82036352b36bb7011099ad2a8a778a99f32dc7
H4ngL <gyeol0719@hanyang.ac.kr>
2024-09-06 15:12:14 +0900

    fix: change quick to heap

--- sort-merge-join/sort_dpu.c
+++ sort-merge-join/sort_dpu.c
@@ -19,47 +19,48 @@ __host dpu_block_t bl;
 uint32_t addr[NR_TASKLETS];
 int rows[NR_TASKLETS];
 
-void quick_sort(uint32_t addr, int row_num, int col_num, int key, int *tasklet_row_arr, int *temp_i_arr, int *temp_j_arr)
+void heapify(uint32_t addr, int row_num, int col_num, int key, int *root_val, int *child_val)
 {
-    if (row_num <= 1)
-        return;
-
-    int offset = (row_num / 2) * col_num * sizeof(int);
-    mram_read((__mram_ptr void const *)(addr + offset), tasklet_row_arr, col_num * sizeof(int));
-
-    int pivot = tasklet_row_arr[key];
-    int i = 0;
-    int j = row_num - 1;
-
-    while (i <= j)
+    for (int i = 1; i < row_num; ++i)
     {
-        mram_read((__mram_ptr void const *)(addr + i * col_num * sizeof(int)), temp_i_arr, col_num * sizeof(int));
-        mram_read((__mram_ptr void const *)(addr + j * col_num * sizeof(int)), temp_j_arr, col_num * sizeof(int));
-
-        while (temp_i_arr[key] < pivot && i <= j)
+        int child = i;
+        do
         {
-            i++;
-            mram_read((__mram_ptr void const *)(addr + i * col_num * sizeof(int)), temp_i_arr, col_num * sizeof(int));
-        }
-
-        while (temp_j_arr[key] > pivot && i <= j)
-        {
-            j--;
-            mram_read((__mram_ptr void const *)(addr + j * col_num * sizeof(int)), temp_j_arr, col_num * sizeof(int));
-        }
-
-        if (i <= j)
-        {
-            mram_write(temp_i_arr, (__mram_ptr void *)(addr + j * col_num * sizeof(int)), col_num * sizeof(int));
-            mram_write(temp_j_arr, (__mram_ptr void *)(addr + i * col_num * sizeof(int)), col_num * sizeof(int));
-
-            i++;
-            j--;
-        }
+            int root = (child - 1) / 2;
+            int offset = root * col_num * sizeof(int);
+            mram_read((__mram_ptr void *)(addr + offset), root_val, col_num * sizeof(int));
+            mram_read((__mram_ptr void *)(addr + child * col_num * sizeof(int)), child_val, col_num * sizeof(int));
+            if (root_val[key] < child_val[key])
+            {
+                mram_write(root_val, (__mram_ptr void *)(addr + child * col_num * sizeof(int)), col_num * sizeof(int));
+                mram_write(child_val, (__mram_ptr void *)(addr + offset), col_num * sizeof(int));
+            }
+            child = root;
+        } while (child != 0);
     }
+}
 
-    quick_sort(addr, j + 1, col_num, key, tasklet_row_arr, temp_i_arr, temp_j_arr);
-    quick_sort(addr + i * col_num * sizeof(int), row_num - i, col_num, key, tasklet_row_arr, temp_i_arr, temp_j_arr);
+void heap(uint32_t addr, int *row_num, int col_num, int key, int *temp_i, int *temp_j)
+{
+    mram_read((__mram_ptr void *)(addr), temp_i, col_num * sizeof(int));
+    mram_read((__mram_ptr void *)(addr + (*row_num - 1) * col_num * sizeof(int)), temp_j, col_num * sizeof(int));
+    mram_write(temp_i, (__mram_ptr void *)(addr + (*row_num - 1) * col_num * sizeof(int)), col_num * sizeof(int));
+    mram_write(temp_j, (__mram_ptr void *)(addr), col_num * sizeof(int));
+
+    --(*row_num);
+}
+
+void heap_sort(uint32_t addr, int row_num, int col_num, int key)
+{
+    int *root_val = (int *)mem_alloc(col_num * sizeof(int));
+    int *child_val = (int *)mem_alloc(col_num * sizeof(int));
+    int size = row_num;
+
+    for (int i = 0; i < size; ++i)
+    {
+        heapify(addr, row_num, col_num, key, root_val, child_val);
+        heap(addr, &row_num, col_num, key, root_val, child_val);
+    }
 }
 
 int main()
@@ -84,11 +85,7 @@ int main()
 
     /* do quick sort */
 
-    int *tasklet_row_arr = (int *)mem_alloc(col_num * sizeof(int));
-    int *temp_i_arr = (int *)mem_alloc(col_num * sizeof(int));
-    int *temp_j_arr = (int *)mem_alloc(col_num * sizeof(int));
-
-    quick_sort(addr[tasklet_id], rows[tasklet_id], col_num, JOIN_KEY, tasklet_row_arr, temp_i_arr, temp_j_arr);
+    heap_sort(addr[tasklet_id], rows[tasklet_id], col_num, JOIN_KEY);
     barrier_wait(&my_barrier);
 
     /* do merge sort */

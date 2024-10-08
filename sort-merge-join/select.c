abe9f43d7eedba4ac163a7d51e22cb02f7bd9308
H4ngL <gyeol0719@hanyang.ac.kr>
2024-10-08 20:24:24 +0900

    modify: comment position

--- sort-merge-join/select.c
+++ sort-merge-join/select.c
@@ -114,13 +114,16 @@ int main()
         {
             bl.row_num += p_count + l_count;
             mem_reset();
+        }
 
 #ifdef DEBUG
-            mutex_lock(my_mutex);
+        mutex_lock(my_mutex);
+        if (is_last)
+        {
             printf("Table %d select results: %d\n", bl.table_num, bl.row_num);
-            mutex_unlock(my_mutex);
-#endif
         }
+        mutex_unlock(my_mutex);
+#endif
     }
 
     barrier_wait(&my_barrier);

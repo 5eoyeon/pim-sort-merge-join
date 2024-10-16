681ab0c061994139b76d62777bf84f2f3d7fd068
H4ngL <gyeol0719@hanyang.ac.kr>
2024-10-16 23:46:42 +0900

    add: run.py

--- test/2024-10-15/common.h
+++ test/2024-10-15/common.h
@@ -1,16 +1,16 @@
-#define DEBUG
+// #define DEBUG
 #define INT64
-#define FILE_NAME_1 "./data/test_data_100000.csv"
-#define FILE_NAME_2 "./data/test_data_100000.csv"
+// #define FILE_NAME_1 "./data/test_data_200000.csv"
+// #define FILE_NAME_2 "./data/test_data_200000.csv"
 
-#define NR_DPUS 64
-#define NR_TASKLETS 11
+#define NR_DPUS 5
+#define NR_TASKLETS 2
 
 #define SELECT_COL1 2
-#define SELECT_VAL1 100
+#define SELECT_VAL1 50
 
 #define SELECT_COL2 2
-#define SELECT_VAL2 100
+#define SELECT_VAL2 50
 
 #define JOIN_KEY1 0
 #define JOIN_KEY2 0

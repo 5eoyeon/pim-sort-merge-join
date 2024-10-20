6e5670ca451a9797c60592ec3af502131210b31b
5eoyeon <tjdus092222@gmail.com>
2024-10-20 20:40:54 +0900

    modify: data

--- test/data/generate_data.py
+++ test/data/generate_data.py
@@ -1,15 +1,26 @@
 import pandas as pd
 import numpy as np
 
-num_rows = 300000
+num_rows = 1000000
 num_cols = 4
 
-data = np.random.randint(1, 1000000, size=(num_rows, num_cols - 1))
+data = np.random.randint(1, num_rows * 3, size=(num_rows, num_cols - 1))
 
-unique_col = np.random.choice(range(1, num_rows + 1), size=num_rows, replace=False)
+unique_col = np.random.choice(range(1, (num_rows * 3 + 1)), size=num_rows, replace=False)
 
 data = np.column_stack((unique_col, data))
 
 df = pd.DataFrame(data, columns=[f"col{i+1}" for i in range(num_cols)])
 
-df.to_csv('test_data(1).csv', index=False)
+df.to_csv('data_100.csv', index=False)
+
+
+data = np.random.randint(1, num_rows * 3, size=(num_rows, num_cols - 1))
+
+unique_col = np.random.choice(range(1, (num_rows * 3 + 1)), size=num_rows, replace=False)
+
+data = np.column_stack((unique_col, data))
+
+df = pd.DataFrame(data, columns=[f"col{i+1}" for i in range(num_cols)])
+
+df.to_csv('data_100(1).csv', index=False)
